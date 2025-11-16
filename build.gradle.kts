68d938176129305c3e0f7eec1274b2241fbe3bfb
Przemyslaw Swiderski <przemyslaw.swiderski7@gmail.com>
2025-11-16 03:26:13 +0100

    redundant versions plugin removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -6,7 +5,6 @@ plugins {
     alias(libs.plugins.kotlin)
     alias(libs.plugins.kover)
     alias(libs.plugins.pluginPublish)
-    alias(libs.plugins.pluginVersions)
 }
 
 version = System.getenv("PLUGIN_VERSION") ?: "unspecified"
@@ -36,17 +34,6 @@ tasks {
         }
     }
 
-    named<DependencyUpdatesTask>("dependencyUpdates").configure {
-        rejectVersionIf {
-            fun isNonStable(version: String): Boolean {
-                val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-                val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-                val isStable = stableKeyword || regex.matches(version)
-                return isStable.not()
-            }
-            isNonStable(candidate.version)
-        }
-    }
 }
 
 
